6dc465c7860f3dcab94fc2a7aa3d5015aa44d081
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-01-29 20:05:01 +0100

    wip

--- page-templates/marketing-activiteiten.php
+++ page-templates/marketing-activiteiten.php
@@ -10,7 +10,7 @@ $current_user_id = get_current_user_id();
 ?>
 <main>
     <div class="px-2 max-w-[680px] mx-auto">
-       <?php echo do_shortcode( '[gravityform id="1"]' ); ?>
+       <?php echo do_shortcode( '[gravityform id="2" title="false"]' ); ?>
     </div>
 </main>
 <?php get_footer('nofooter'); ?>
