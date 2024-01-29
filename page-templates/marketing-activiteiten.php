6db928de68b57871220691d9cc894edef9df0fa5
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-01-29 19:41:06 +0100

    wip

---
+++ page-templates/marketing-activiteiten.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template name: Marketing activiteiten
+ */
+
+
+ get_header(); ?>
+<?php
+$current_user_id = get_current_user_id();
+?>
+<main>
+    <div class="px-2 max-w-[680px] mx-auto">
+       <?php echo do_shortcode( '[gravityform id="1"]' ); ?>
+    </div>
+</main>
+<?php get_footer('nofooter'); ?>
+
+
+
