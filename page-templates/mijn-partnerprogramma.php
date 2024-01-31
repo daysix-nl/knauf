72e439be1a6e6fa0898575dcdb46c2d67c6e3857
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-01-31 10:16:17 +0100

    wip

--- page-templates/mijn-partnerprogramma.php
+++ page-templates/mijn-partnerprogramma.php
@@ -41,10 +41,36 @@ $current_user = wp_get_current_user();
                 <h2 class="text-24 leading-24 text-[#001A4C] mb-2">Mijn marketing activiteiten</h2>
                 <p class="font-light text-[#001A4C]"><?php the_field('marketing_activiteiten', 'user_' . $current_user_id); ?></p>
                 <?php if (get_field('type_partner', 'user_' . $current_user_id) === "Promotor (Level 1)"): ?>   
-                <a href="https://partner.knaufmarketing.nl/marketing-activiteiten/?c=1&voornaam=<?php echo esc_html( $current_user->user_firstname ) ?>&achternaam=<?php echo esc_html( $current_user->user_lastname ) ?>&email=<?php echo esc_html( $current_user->user_email ) ?>&k<?php echo esc_html( $current_user->user_login )?>">Klik hier</a>
+                <a href="https://partner.knaufmarketing.nl/marketing-activiteiten/?c=1&voornaam=<?php echo esc_html( $current_user->user_firstname ) ?>&achternaam=<?php echo esc_html( $current_user->user_lastname ) ?>&email=<?php echo esc_html( $current_user->user_email ) ?>&k<?php echo esc_html( $current_user->user_login )?>">
+                <div class="flex mt-[30px]">
+                    <p class="font-medium text-[#009FE3] mr-[10px]">Bestellen</p>
+                    <div class="h-[24px] w-[24px] flex items-center">
+                        <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Group" fill="#009FE3" fill-rule="nonzero">
+                                    <polygon id="Path" points="8 0 6.59 1.41 12.17 7 0 7 0 9 12.17 9 6.59 14.59 8 16 16 8"></polygon>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                </a>
                 <?php endif; ?>
                 <?php if (get_field('type_partner', 'user_' . $current_user_id) === "Ambassadeur (Level 2)"): ?>   
-                <a href="https://partner.knaufmarketing.nl/marketing-activiteiten/?c=1&voornaam=<?php echo esc_html( $current_user->user_firstname ) ?>&achternaam=<?php echo esc_html( $current_user->user_lastname ) ?>&email=<?php echo esc_html( $current_user->user_email ) ?>&k<?php echo esc_html( $current_user->user_login )?>">Klik hier</a>
+                <a href="https://partner.knaufmarketing.nl/marketing-activiteiten/?c=1&voornaam=<?php echo esc_html( $current_user->user_firstname ) ?>&achternaam=<?php echo esc_html( $current_user->user_lastname ) ?>&email=<?php echo esc_html( $current_user->user_email ) ?>&k<?php echo esc_html( $current_user->user_login )?>">
+                 <div class="flex mt-[30px]">
+                    <p class="font-medium text-[#009FE3] mr-[10px]">Bestellen</p>
+                    <div class="h-[24px] w-[24px] flex items-center">
+                        <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Group" fill="#009FE3" fill-rule="nonzero">
+                                    <polygon id="Path" points="8 0 6.59 1.41 12.17 7 0 7 0 9 12.17 9 6.59 14.59 8 16 16 8"></polygon>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                </a>
                 <?php endif; ?>
             </div>
             <div class="w-full min-h-[250px] px-[25px]  schaduw py-[30px] order-1 md:order-2">
